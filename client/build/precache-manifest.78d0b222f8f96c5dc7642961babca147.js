855a95a53e7800db904a34e4062b304f04fe71e2
fran <francisco.chiarino@gmail.com>
2020-07-23 20:15:50 +0200

    Created build

---
+++ client/build/precache-manifest.78d0b222f8f96c5dc7642961babca147.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b09a9abe5c59633f54cca4f0acd5429",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59caf55ea9ea227f57c3",
+    "url": "/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "0247ef7a83c2f2e3a0a3",
+    "url": "/static/css/main.c91919de.chunk.css"
+  },
+  {
+    "revision": "59caf55ea9ea227f57c3",
+    "url": "/static/js/2.f736e164.chunk.js"
+  },
+  {
+    "revision": "bebc64d23ee3fac4a59b5fb04d845837",
+    "url": "/static/js/2.f736e164.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0247ef7a83c2f2e3a0a3",
+    "url": "/static/js/main.5a1dba61.chunk.js"
+  },
+  {
+    "revision": "6a06dd8c58e6064993b5",
+    "url": "/static/js/runtime-main.83421ab0.js"
+  }
+]);

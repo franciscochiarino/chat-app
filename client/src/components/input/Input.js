8f77e4d58804c5faa616ca5447c32285a609aac2
fran <francisco.chiarino@gmail.com>
2020-07-23 19:05:54 +0200

    Implemented textarea autosize

--- client/src/components/input/Input.js
+++ client/src/components/input/Input.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import 'emoji-mart/css/emoji-mart.css';
 import { Picker, Emoji } from 'emoji-mart';
+import TextareaAutosize from 'react-autosize-textarea';
 
 export default function Input({ text, setText, sendMessage }) {
   const [emojiWindow, setEmojiWindow] = useState(false);
@@ -17,7 +18,8 @@ export default function Input({ text, setText, sendMessage }) {
       <div className="Input">
         <form onSubmit={sendMessage} className="chat-form">
           <button type="button" className="emoji-btn" onClick={handleEmojiWindow}><Emoji emoji={{ id: 'grinning'}} size={20} /></button>
-          <input type="text" placeholder="Type a message..." value={text} onChange={(e) => setText(e.target.value)} />
+          {/* <input type="text" placeholder="Type a message..." value={text} onChange={(e) => setText(e.target.value)} /> */}
+          <TextareaAutosize className="textarea-autosize" style={{ minHeight: 20, maxHeight: 70, resize: 'none' }} placeholder="Type a message..." value={text} onChange={(e) => setText(e.target.value)} />
           <button type="submit" onClick={() => setEmojiWindow(false)}>Send</button>
         </form>
       </div>

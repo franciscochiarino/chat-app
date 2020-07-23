697850f55ab9ab89b1a5f5b30b26ce2d6dd2ded7
fran <francisco.chiarino@gmail.com>
2020-07-23 14:40:21 +0200

    Emojis can only work with Arial

--- client/src/Input.js
+++ client/src/Input.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
+import 'emoji-mart/css/emoji-mart.css';
+import { Picker, getEmojiDataFromNative } from 'emoji-mart';
 
 export default function Input({ text, setText, sendMessage }) {
   return (
+    <>
+    <Picker set="apple" theme="light" style={{fontFamily: 'initial'}} onSelect={emoji => setText(text + emoji.native)} />
     <div className="Input">
       <form onSubmit={sendMessage} className="chat-form">
         <input type="text" placeholder="Type a message..." value={text} onChange={(e) => setText(e.target.value)} />
         <button type="submit">Send</button>
       </form>
     </div>
+    </>
   )
 }

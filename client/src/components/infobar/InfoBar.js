1cc8d66862089e7789659a53c606b22ef29b3620
fran <francisco.chiarino@gmail.com>
2020-07-23 19:29:20 +0200

    Added close button

--- client/src/components/infobar/InfoBar.js
+++ client/src/components/infobar/InfoBar.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import closeBtn from '../../icons/close.png'
 
 export default function InfoBar({ room }) {
+
+  const handleCloseBtn = () => {
+    sessionStorage.removeItem('name');
+    window.location = '/';
+  }
+
   return (
     <div className="InfoBar">
       <h1>{room}</h1>
+      <img onClick={handleCloseBtn} src={closeBtn} className="close-chat-btn" alt="close" />
     </div>
   )
 }
